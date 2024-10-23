845ea80f975bd4bcd9f7b6a9dfa9067010240c6e
hilman <hpanser@gmail.com>
2024-10-23 20:53:47 +0700

    update

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,14 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/Logo.png'
+import themeIcon from '../../assets/sun.svg'
 
 function Hero() {
   return (
     <section id='hero'>
-      <div><img src={heroImg} alt="" /></div>
+      <div className={styles.colorModeContainer}>
+        <img className={styles.hero} src={heroImg} alt="JustHilman Profile Picture" />
+        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" />
+      </div>
     </section>
   )
 }
