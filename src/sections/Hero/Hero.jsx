e08a6e61a4401758be89fcdb22462eea7fdf87f5
hilman <hpanser@gmail.com>
2024-10-26 20:57:24 +0700

    update common styles

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,17 +1,29 @@
 import styles from './HeroStyles.module.css'
 import heroImg from '../../assets/Logo.png'
-import themeIcon from '../../assets/sun.svg'
-import twitterIcon from '../../assets/twitter-light.svg'
-import githubIcon from '../../assets/github-light.svg'
-import linkedInIcon from '../../assets/linkedin-light.svg'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
+import twitterLight from '../../assets/twitter-light.svg'
+import twitterDark from '../../assets/twitter-dark.svg'
+import githubLight from '../../assets/github-light.svg'
+import githubDark from '../../assets/github-dark.svg'
+import linkedInLight from '../../assets/linkedin-light.svg'
+import linkedInDark from '../../assets/linkedin-dark.svg'
 import CV from '../../assets/Logo.png'
+import { useTheme } from '../../common/ThemeContext'
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedInIcon = theme === 'light' ? linkedInLight : linkedInDark;
+  
   return (
     <section id='hero' className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img className={styles.hero} src={heroImg} alt="JustHilman Profile Picture" />
-        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" />
+        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" onClick={toggleTheme}/>
       </div>
       <div className={styles.info}>
         <h1>Just <br /> Hilman</h1>
@@ -27,7 +39,7 @@ function Hero() {
             <img src={linkedInIcon} alt="LinkedIn Icon" />
           </a>
         </span>
-        <p>
+        <p className={styles.description}>
           With a passion for developing modern web apps for commercial bussiness.
         </p>
         <a href={CV} download>
